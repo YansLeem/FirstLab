e4ff77060f446f4bf775839e502563eb2df46dc2
YansLeem <yans1996@yandex.ru>
2019-02-08 16:20:50 +0300

    my first commit

---
+++ src/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+    public static void main(String[] args){
+        System.out.println("Hello mthrfckng World!");
+        System.out.println("Hello mthrfckng Wo!");
+    }
+}
